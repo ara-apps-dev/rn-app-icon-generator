c0454b78f567369e2e1c8b80ccfc230dfa28175e
guska <ipg.guska.p@gmail.com>
2025-06-16 11:25:03 +0800

    v1.0.1 - Add CLI tool with icon generator for iOS and Android

--- bin/index.js
+++ bin/index.js
@@ -1,9 +1,13 @@
 #!/usr/bin/env node
 
-const fs = require("fs-extra");
-const path = require("path");
-const sharp = require("sharp");
-const chalk = require("chalk");
+import fs from "fs-extra";
+import path from "path";
+import sharp from "sharp";
+import chalk from "chalk";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 console.log(chalk.green("Running rn-app-icon-generator CLI..."));
 
